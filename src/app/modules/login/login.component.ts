461f59b302fb9b15846308f87592630ce579f62a
LivPasc <266094@via.dk>
2020-12-16 15:40:42 +0100

    Adding snakbar for login and notification

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,43 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/shared/authentication.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  
   formModel = {
     username: '',
-    password: ''
-  }
+    password: '',
+  };
 
-  constructor(private service: AuthenticationService,private router: Router) { }
+  constructor(
+    private service: AuthenticationService,
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
-    if (localStorage.getItem('token') != null)
-    this.router.navigateByUrl('');
+    if (localStorage.getItem('token') != null) this.router.navigateByUrl('');
   }
 
-
   onSubmit(form: NgForm) {
-    console.log(JSON.stringify(form.value))
+    console.log(JSON.stringify(form.value));
     this.service.login(form.value).subscribe(
       (res: any) => {
         localStorage.setItem('token', res.token);
         this.router.navigateByUrl('');
         window.location.reload();
       },
-      err => {
-        if (err.status == 400)
-         console.log("smth went wrong");
-        else
-          console.log(err);
+      (err) => {
+        this._snackBar.open('Login failed', 'Dismiss', {
+          duration: 2000,
+        });
       }
     );
-
   }
 }
